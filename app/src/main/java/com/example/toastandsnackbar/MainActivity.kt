21c3ac691ae1abc2209f002dfb48b360e38b6692
dzaky1201 <dzakyhdr@gmail.com>
2022-01-17 12:40:34 +0700

    mini-case

--- app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
+++ app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.toastandsnackbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import android.widget.Toast
 import com.example.toastandsnackbar.databinding.ActivityMainBinding
@@ -10,22 +11,27 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var countDownTimer: CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+       countDownTimer = object : CountDownTimer(10000, 1000) {
+            override fun onTick(p0: Long) {
+                binding.timer.text = getString(R.string.time_reamining, p0 / 1000)
+            }
 
-        binding.btnSnackbar.setOnClickListener {
-            val snackbar = Snackbar.make(binding.root, "berhasil muncul", Snackbar.LENGTH_INDEFINITE)
-            snackbar.setAction("Retry"){
-                Snackbar.make(binding.root, "retry di pencet", Snackbar.LENGTH_SHORT).show()
-            }.show()
+            override fun onFinish() {
+                Snackbar.make(binding.root, "Waktu Sudah habis", Snackbar.LENGTH_LONG).setAction("Retry"){
+                    countDownTimer.start()
+                }.show()
+            }
         }
 
-        binding.btnToast.setOnClickListener {
-            Toast.makeText(this, "Button Toast Di click", Toast.LENGTH_SHORT).show()
+        binding.btnStart.setOnClickListener {
+            countDownTimer.start()
         }
     }
 }
